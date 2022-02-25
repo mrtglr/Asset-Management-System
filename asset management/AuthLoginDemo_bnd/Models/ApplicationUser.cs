8386acb77fc05f259ce865a081646d60937abd89
Mert Güler <78295915+mrtglr@users.noreply.github.com>
2022-02-25 20:22:58 +0300

    back-end update ver1

---
+++ asset management/AuthLoginDemo_bnd/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthLoginDemo_bnd.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
---
+++ asset management/AuthLoginDemo_bnd/Models/ApplicationUserModel.cs
@@ -0,0 +1,10 @@
+namespace AuthLoginDemo_bnd.Models
+{
+    public class ApplicationUserModel
+    {
+        public string UserName {get; set;}
+        public string Email {get; set;}
+        public string Password {get; set;}
+        public string Fullname {get; set;}
+    }
+}
